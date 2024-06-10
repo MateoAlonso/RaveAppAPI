f60c276a29ad9031ffc8e5966179a577525a0627
MateoAlonso <mateoalonsogioia27@gmail.com>
2024-06-10 00:00:13 -0300

    V 1.0.0.6 feat: agrega helper de base de datos feat: agrega hasher para passwords

---
+++ Services/Helpers/DbHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaveAppAPI.Services.Helpers
+{
+    public static class DbHelper
+    {
+        public static string GetConnectionString()
+        {
+#if DEBUG
+        return Environment.GetEnvironmentVariable("dbcon", EnvironmentVariableTarget.Machine);
+#else
+        return Environment.GetEnvironmentVariable("dbcon");
+#endif
+        }          
+    }
+}
---
+++ Services/Helpers/Hasher.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaveAppAPI.Services.Helpers
+{
+    // De https://learn.microsoft.com/en-us/aspnet/core/security/data-protection/consumer-apis/password-hashing?view=aspnetcore-8.0
+    public static class Hasher
+    {
+        public static string HashPassword(string password)
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+            Console.WriteLine($"Salt: {Convert.ToBase64String(salt)}");
+
+           return Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password!,
+                salt: salt,
+                prf: KeyDerivationPrf.HMACSHA256,
+                iterationCount: 100000,
+                numBytesRequested: 256 / 8));
+        }
+        
+    }
+}
