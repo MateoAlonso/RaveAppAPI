dfb9f6b724278d51033a223d990f1d7391257f48
MateoAlonso <mateoalonsogioia27@gmail.com>
2025-03-15 22:20:24 -0300

    V 1.0.0.9 chore: cambia numero de version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.0.8")]
-[assembly: AssemblyFileVersion("1.0.0.8")]
+[assembly: AssemblyVersion("1.0.0.9")]
+[assembly: AssemblyFileVersion("1.0.0.9")]
 [assembly: AssemblyProduct("RaveAppAPI")]
 [assembly: AssemblyDescription("Disenado y desarrollado por Mateo Alonso Gioia")]
