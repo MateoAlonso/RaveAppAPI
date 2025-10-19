8829abf612ac5361b0655d092ed09b51a3fc03f4
MateoAlonso <mateoalonsogioia27@gmail.com>
2025-10-19 19:23:05 -0300

    feat: usuarios de control entradas fix: 403 response en pass incorrecta
    Se cambia logica de login para loguear usuarios y usuarios de control en la misma ruta

---
+++ Services/Models/GetUsuariosControlDTO.cs
@@ -0,0 +1,12 @@
+﻿using RaveAppAPI.Services.Helpers;
+
+namespace RaveAppAPI.Services.Models
+{
+    public class GetUsuariosControlDTO
+    {
+        [ColumnName("IdUsuarioControl")]
+        public string IdUsuarioControl { get; set; }
+        [ColumnName("dsnombreusuario")]
+        public string NombreUsuario { get; set; }
+    }
+}
